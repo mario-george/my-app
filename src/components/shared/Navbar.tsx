8e0a4dc257d9332eb417c49babe3a2eccd239c2c
mario-george <vortexspades@gmail.com>
2024-01-03 01:37:07 +0200

    adding the array i will be rendering in the navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -20,7 +20,21 @@ export default function Navbar() {
     );
   }, []);
 
-  
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        placeholder={``}
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center text-lg">
+          Users
+        </a>
+      </Typography>
+    </ul>
+  );
 
   return <></>
 }

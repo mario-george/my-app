07ddafed77529e68864f8b6dad26a13d81f14ad2
mario-george <vortexspades@gmail.com>
2024-01-03 01:34:46 +0200

    adding imports of the components that will be used for the navbar

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react";
+import {
+  Navbar as NavbarEl,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+  Card,
+} from "./material-tailwind";
+
+export default function Navbar() {
+ return <></>
+}

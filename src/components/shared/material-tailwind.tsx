2f4c5aa6f300093f148e9a3a2b340a127237359a
mario-george <vortexspades@gmail.com>
2024-01-02 19:45:21 +0200

    using a seperate file to import material-tailwind components

---
+++ src/components/shared/material-tailwind.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import {
+  ThemeProvider,
+  Button,
+  Card,
+  Input,
+  Checkbox,
+  Typography,
+} from "@material-tailwind/react";
+
+export { ThemeProvider, Card, Input, Checkbox, Button, Typography };

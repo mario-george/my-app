117882fb2760c0cc126014e74caff75d277a7a1f
mario-george <vortexspades@gmail.com>
2024-01-03 01:10:01 +0200

    update root layout to use the external wrapper layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
+
+import Layout from '@/components/shared/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="bg-gray-50">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className=" ">
+      <body className={inter.className}>
+        <Layout>
+
+        {children}
+        </Layout>
+        </body>
     </html>
   )
 }

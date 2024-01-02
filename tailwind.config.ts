b7182f22aec86509d267f00ad6b4e58a969aa3cd
mario-george <vortexspades@gmail.com>
2024-01-02 19:46:19 +0200

    setting up material-tailwind/react to work with tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,4 +19,4 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
+export default withMT(config)

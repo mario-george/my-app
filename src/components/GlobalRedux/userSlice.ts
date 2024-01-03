1be698a1eaea37e6ef46234b0aa9da886d6a5854
mario-george <vortexspades@gmail.com>
2024-01-03 19:15:05 +0200

    adding reducer functions and exporting them from the attribute actions from the created slice

--- src/components/GlobalRedux/userSlice.ts
+++ src/components/GlobalRedux/userSlice.ts
@@ -16,9 +16,15 @@ const userSlice = createSlice({
   
   //This object contains the reducer functions for this slice of the Redux store. Reducer functions specify how the state should be updated in response to certain actions
   reducers: {
-
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
   },
 });
 
+export const { login, logout } = userSlice.actions;
 
 export default userSlice.reducer;

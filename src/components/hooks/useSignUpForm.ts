8f0633654deaabc4b5b64d6a27dc5f893f6dbbf3
mario-george <vortexspades@gmail.com>
2024-01-04 14:38:22 +0200

    add validation for the name and email and the password

--- src/components/hooks/useSignUpForm.ts
+++ src/components/hooks/useSignUpForm.ts
@@ -17,7 +17,21 @@ export default function useSignUpForm() {
   const [errors, setErrors] = useState<Errors>({});
 
   const validateForm = () => {
-
+    let errors: Errors = {};
+    if (!formState.name) {
+      errors.name = "Name is required.";
+    }
+    if (!formState.email) {
+      errors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(formState.email)) {
+      errors.email = "Email is invalid.";
+    }
+    if (!formState.password) {
+      errors.password = "Password is required.";
+    } else if (formState.password.length < 6) {
+      errors.password = "Password must be at least 6 characters.";
+    }
+    setErrors(errors);
   };
 
   // if you used event.target.name it is treated as a string “event.target.name”, not the value it holds (like ‘name’, ‘email’, or ‘password’). So, instead of updating the corresponding field in the formState, it would attempt to update a field literally named “event.target.name”, which is not what you want.

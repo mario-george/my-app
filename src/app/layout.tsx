84144961115426e2b26b0df4fa61e466d9600089
mario-george <vortexspades@gmail.com>
2024-01-04 14:19:56 +0200

    add radix-ui/themes to the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
 
 import './globals.css'
+import '@radix-ui/themes/styles.css';
 
 import Layout from '@/components/shared/Layout'
+
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,10 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en" className=" ">
       <body className={inter.className}>
+      <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
         <Layout>
 
         {children}
         </Layout>
+</Theme>
         </body>
     </html>
   )

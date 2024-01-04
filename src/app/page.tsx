d79316ed1d82ac3a0cd90b15a09ed63e8d7e42c3
mario-george <vortexspades@gmail.com>
2024-01-04 14:36:56 +0200

    render the userList component in the home page by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Users from "@/components/users/usersList";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return <main className=" mx-auto w-1/2 grid grid-cols-2 container">
+    <Users/>
+  </main>;
 }

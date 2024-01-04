4150304f12aa835f94267586b37af345144430a1
mario-george <vortexspades@gmail.com>
2024-01-04 14:22:28 +0200

    add a component to render the user list each one with a card

---
+++ src/components/users/usersList.tsx
@@ -0,0 +1,19 @@
+import { Avatar, Box, Card, Flex,Text } from "@radix-ui/themes";
+
+
+export default function Users() {
+
+
+interface user{
+    name:string,
+    image:string,
+    places:number,
+}
+
+  let users: user[] = [{},{},{}];
+  return (
+    <>
+     
+    </>
+  );
+}

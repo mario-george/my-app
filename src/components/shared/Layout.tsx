3c7ec28006555a9ec36960da87816f483195afa4
mario-george <vortexspades@gmail.com>
2024-01-04 19:10:08 +0200

    state management and theme from radix-ui in a seperate component

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,11 +1,25 @@
+"use client";
+import { store } from "@/components/GlobalRedux/store";
+import { Provider } from "react-redux";
 import React from "react";
+import { Theme } from "@radix-ui/themes";
+
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
-      {children}
+      <Theme
+        accentColor="crimson"
+        grayColor="sand"
+        radius="large"
+        scaling="95%"
+      >
+        <Provider store={store}>
+          <Navbar />
+          {children}
+        </Provider>
+      </Theme>
     </>
   );
 }

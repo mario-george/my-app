87305e76cbb6f5813282f7ec67226846f9c1fb4d
mario-george <vortexspades@gmail.com>
2024-01-04 14:23:26 +0200

    rendering a card component for each user of the array

--- src/components/users/usersList.tsx
+++ src/components/users/usersList.tsx
@@ -13,7 +13,26 @@ interface user{
   let users: user[] = [{},{},{}];
   return (
     <>
-     
+      {users.map((u:user) => {
+        return <Card style={{ maxWidth: 240 }} className="my-2">
+        <Flex gap="3" align="center">
+          <Avatar
+            size="3"
+            src={u.image||'/images/defaultImage.svg' }
+            radius="full"
+            fallback="M"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Mario George
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Flex>
+      </Card>;
+      })}
     </>
   );
 }

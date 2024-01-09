20595482ab8bdaa990b0766f4d6e7574beaeb829
mario-george <vortexspades@gmail.com>
2024-01-09 10:51:38 +0200

    formatted by prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Users from "@/components/users/usersList";
 import Image from "next/image";
-
 export default function Home() {
-  return <main className=" mx-auto w-1/2 grid grid-cols-2 container">
-    <Users/>
-  </main>;
+  return (
+    <main className=" mx-auto w-1/2 grid grid-cols-2 container">
+      <Users />
+    </main>
+  );
 }

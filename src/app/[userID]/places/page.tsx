f44b377c0da500a112f8f9039d144293692bc43c
mario-george <vortexspades@gmail.com>
2024-01-09 19:49:38 +0200

    adding imports for place

--- src/app/[userID]/places/page.tsx
+++ src/app/[userID]/places/page.tsx
@@ -1,1 +1,34 @@
 // display list of user places (and allow edit and delete only for the authenticated user or authorized user)
+"use client";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Input,
+  Button,
+  Textarea,
+} from "@nextui-org/react";
+
+
+const Place = (props) => {
+ 
+
+  useEffect(() => {
+   
+  }, []);
+
+  const deletePlaceHandler = async () => {
+   
+  };
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Place;

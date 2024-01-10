1a6b0895e97ffd575dff0c252f1ed3fde872a07d
mario-george <vortexspades@gmail.com>
2024-01-10 16:30:19 +0200

    Add Spinner and Error Handling to Place Component

--- src/app/[userID]/places/page.tsx
+++ src/app/[userID]/places/page.tsx
@@ -1,7 +1,9 @@
 // display list of user places (and allow edit and delete only for the authenticated user or authorized user)
 "use client";
-import { useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Spinner } from "@nextui-org/react";
+
 import PlaceCard from "@/components/UI/PlaceCard";
 import {
   Card,
@@ -27,19 +29,43 @@ interface RootState {
     loggedIn?: boolean | null;
   };
 }
+interface PlaceType {
+  image: string;
+  description: string;
+  title: string;
+  address: string;
+  id: string;
+}
 const Place = (props: Props) => {
-  const [places, setPlaces] = useState([]);
+  const [places, setPlaces] = useState<Array<PlaceType> | undefined>();
   const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
   const GlobalStateUser = useSelector((state: RootState) => state.user.user);
 
+  const [content, setContent] = useState<ReactElement>(
+    <div className="fixed inset-0 flex justify-center items-center">
+      <Spinner label="Loading..." color="warning" size="lg" className="" />
+    </div>
+  );
+
   useEffect(() => {
     let fetchPlaces = async () => {
-      const resp = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}places/user/${props.params.userID}`
-      );
-      const respData = await resp.json();
-      console.log(respData);
-      setPlaces(respData.places);
+      try {
+        const resp = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}places/user/${props.params.userID}`
+        );
+        const respData = await resp.json();
+        if (!resp.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        setPlaces(respData.places);
+      } catch (err) {
+        setContent(
+          <div className="fixed inset-0 flex justify-center items-center">
+            <p>No places found</p>
+          </div>
+        );
+      }
     };
     // Fetch data from external API
     fetchPlaces();
@@ -48,27 +74,24 @@ const Place = (props: Props) => {
     }
   }, []);
 
-
-
   return (
     <>
-      {places.length !== 0 ? (
-        places.map((p) => {
-          const { image, description, title, id, address } = p;
-          return (
-            <PlaceCard
-              image={image}
-              description={description}
-              title={title}
-              address={address}
-              id={id}
-              isAuthorized={isAuthorized}
-            />
-          );
-        })
-      ) : (
-        <>No places found for the user</>
-      )}
+      {places == undefined
+        ?  content 
+        : places?.map((p: PlaceType) => {
+            const { image, description, title, id, address } = p;
+            return (
+              <PlaceCard
+                key={p.id}
+                image={image}
+                description={description}
+                title={title}
+                address={address}
+                id={id}
+                isAuthorized={isAuthorized}
+              />
+            );
+          })}
     </>
   );
 };

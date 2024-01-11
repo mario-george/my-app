7b6814de421540c12583190e31f2e712fca0ee12
mario-george <vortexspades@gmail.com>
2024-01-11 21:37:09 +0200

    Add colors and keyframes to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 const {nextui} = require("@nextui-org/react");
 
 const withMT = require("@material-tailwind/react/utils/withMT");
+const { blackA, green, mauve, slate, violet } = require('@radix-ui/colors');
 
 const config: Config = {
   content: [
@@ -12,11 +13,34 @@ const config: Config = {
 
   ],
   theme: {
-    extend: {
+    extend: {colors: {
+      ...blackA,
+      ...green,
+      ...mauve,
+      ...slate,
+      ...violet,
+    },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      }, keyframes: {
+        hide: {
+          from: { opacity: '1' },
+          to: { opacity: '0' },
+        },
+        slideIn: {
+          from: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+          to: { transform: 'translateX(0)' },
+        },
+        swipeOut: {
+          from: { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
+          to: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+        },
+      },   animation: {
+        hide: 'hide 100ms ease-in',
+        slideIn: 'slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        swipeOut: 'swipeOut 100ms ease-out',
       },
     },
   },  darkMode: "class",

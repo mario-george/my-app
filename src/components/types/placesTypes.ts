fa11bbf56909be4ce8e41d65f9911f705c218a15
mario-george <vortexspades@gmail.com>
2024-01-12 22:18:07 +0200

    Add PlaceType interface to placesTypes.ts

---
+++ src/components/types/placesTypes.ts
@@ -0,0 +1,11 @@
+interface PlaceType {
+    location:{
+      lat:number,
+      lng:number
+    }
+    image: string;
+    description: string;
+    title: string;
+    address: string;
+    id: string;
+  }

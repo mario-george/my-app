45bb6562ed6c44ec5e157e49b2f7f19f3427a9f1
mario-george <vortexspades@gmail.com>
2024-01-14 01:34:40 +0200

    Add formTypes.ts with error interfaces

---
+++ src/components/types/formTypes.ts
@@ -0,0 +1,24 @@
+export interface SignUpErrors {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+export interface SignInErrors {
+  email?: string;
+  password?: string;
+  back?: string;
+}
+
+export interface PlaceErrors {
+  title?: string;
+  description?: string;
+  address?: string;
+  back?: string;
+}
+
+export interface AddPlaceErrors {
+  title?: string;
+  address?: string;
+  location?: string;
+  description?: string;
+}

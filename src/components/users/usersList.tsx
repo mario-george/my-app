e61fe085bc79e65da532f8d7b25e47ee538ac18f
mario-george <vortexspades@gmail.com>
2024-05-30 21:52:54 +0300

    Refactor image handling in Users component

--- src/components/users/usersList.tsx
+++ src/components/users/usersList.tsx
@@ -10,6 +10,7 @@ interface User {
   places: Array<any>;
   name: string;
   image: string;
+  imageFileName:string
 }
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -20,7 +21,6 @@ export default function Users() {
       .then((res) => res.json())
       .then((data) => {
         setUsers(data.allUsers);
-        console.log(data.allUsers[0].image);
       });
   }, []);
 
@@ -29,10 +29,17 @@ export default function Users() {
       {users.length !== 0 ? (
         <div className=" w-full grid grid-cols-2 container mx-auto">
           {users?.map((u: User) => {
-            let imageURL =
+            let imageURL
+            if(process.env.NEXT_PUBLIC_AWS_STORAGE === "true"){
+               imageURL =
               process.env.NEXT_PUBLIC_DEV === "true"
                 ? `${process.env.NEXT_PUBLIC_URL_BACKEND}${u.image}`
                 : u.image;
+            }else{
+   imageURL = `${process.env.NEXT_PUBLIC_API_URL+"image/"}${u.imageFileName}`
+
+            }
+          
             console.log(process.env.NEXT_PUBLIC_DEV);
             console.log(imageURL);
             let descriptionInfo =

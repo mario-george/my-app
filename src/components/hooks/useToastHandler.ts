b4e69feb391450741a87c79db004f6cb15a3cc16
mario-george <vortexspades@gmail.com>
2024-05-31 15:33:00 +0300

    Refactor useToastHandler to include custom success toast notifications

--- src/components/hooks/useToastHandler.ts
+++ src/components/hooks/useToastHandler.ts
@@ -5,16 +5,17 @@ import { useToast } from "@chakra-ui/react";
     const toast = useToast();
     
     const toastHandler: (
-        type: "success" | "warning" | "error",
-        message?: string
-      ) => void = (type, message) => {
-        if (type == "success") {
+        type: "place-added" | "warning" | "error"|"custom-success",
+        message?: string,description?:string
+      ) => void = (type, message,description) => {
+        if (type == "place-added") {
           toast({
             title: "Place added.",
             description: "Place has been added successfully.",
             status: "success",
             duration: 9000,
             isClosable: true,
+            position:"bottom-right"
           });
         } else if (type == "warning") {
           toast({
@@ -23,25 +24,46 @@ import { useToast } from "@chakra-ui/react";
             status: "warning",
             duration: 9000,
             isClosable: true,
+            position:"bottom-right"
+
           });
-        } else {
+        } else if (type == "custom-success" && message&&description) {
+            let position=message.includes('Edited') ?"bottom-center":"bottom-right"
+            toast({
+              title:message,
+              description,
+              status: "success",
+              duration: 9000,
+              isClosable: true,
+              position
+  
+            });
+          }else {
           toast({
             title: "Error.",
             description: message,
             status: "error",
             duration: 9000,
             isClosable: true,
-          });
+            position:"bottom-right"
+        });
         }
       };
       let toastCallBack = (message: string) => {
         console.log("message", message);
         if (message == "Invalid Address, Enter a valid address.") {
           toastHandler("warning");
-        }else if(message=="success"){
-            toastHandler("success");
+        }else if(message=="place-added"){
+            toastHandler("place-added");
             
-        } else {
+        } else if(message=="place-deleted"){
+            toastHandler("custom-success","Place Deleted.","Place has been deleted successfully.");
+            
+        }else if(message=="place-edited"){
+            toastHandler("custom-success","Place Edited.","Place has been edited successfully.");
+
+            
+        }else {
           toastHandler("error", message);
         }
       };
